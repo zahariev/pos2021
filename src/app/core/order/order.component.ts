c6260a22d0a2e59dcb6c0bfff6b77edf6cbb15e3
boyan <zaharievboyan@gmail.com>
2021-06-02 10:12:22 +0300

    add: order component

---
+++ src/app/core/order/order.component.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @angular-eslint/component-selector */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'order',
+    templateUrl: './order.component.html',
+    styleUrls: ['./order.component.scss'],
+})
+export class OrderComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
