9a4748fb84391311a99acdaa9da4b2dcddd38883
boyan <zaharievboyan@gmail.com>
2021-06-02 10:12:03 +0300

    add: openbill component

---
+++ src/app/core/openbill-details/openbill-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OpenbillDetailsComponent } from './openbill-details.component';
+
+describe('OpenbillDetailsComponent', () => {
+  let component: OpenbillDetailsComponent;
+  let fixture: ComponentFixture<OpenbillDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OpenbillDetailsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OpenbillDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/openbill-details/openbill-details.component.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @angular-eslint/component-selector */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'openbill-details',
+    templateUrl: './openbill-details.component.html',
+    styleUrls: ['./openbill-details.component.scss'],
+})
+export class OpenbillDetailsComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
