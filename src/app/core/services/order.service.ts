b27ab7d1e479dce439cec124a7edc2899970a24e
boyan <zaharievboyan@gmail.com>
2021-06-04 02:11:53 +0300

    fix: void in middle of array

--- src/app/core/services/order.service.ts
+++ src/app/core/services/order.service.ts
@@ -25,13 +25,13 @@ export class OrderService {
         const lastItem = this.items[this.items.length - 1];
         if (this.items.indexOf(item) > -1) {
             item.qty += qty;
-            if (item.qty <= 0) this.items.pop();
+            if (item.qty <= 0) this.items.splice(this.items.indexOf(item), 1);
         } else if (lastItem?.id === item.id) {
             lastItem.qty = lastItem.qty + qty;
             if (item.qty <= 0) this.items.pop();
         } else {
             item.qty = qty;
-            this.items.push({ ...item });
+            if (item.qty > 0) this.items.push({ ...item });
         }
 
         this.qty = 1;
